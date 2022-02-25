e88318e4c7f0e347133332d6b0f26448cf9237d6
gaclo <gaclo@192.168.0.13>
2022-02-26 00:54:20 +0900

    aa

---
+++ src/main/java/com/trader/AppConfig.java
@@ -0,0 +1,34 @@
+package com.trader;
+
+import java.util.Optional;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+        return server ->
+            Optional.ofNullable(server)
+                    .ifPresent(s -> s.addAdditionalTomcatConnectors(redirectConnector()));
+    }
+ 
+    @SuppressWarnings("rawtypes")
+	private Connector redirectConnector() {
+        Connector connector = new Connector("AJP/1.3");
+        connector.setScheme("http");
+        connector.setPort(8009);
+        connector.setRedirectPort(8443);
+        connector.setSecure(false);
+        connector.setAllowTrace(false);
+        ((AbstractAjpProtocol) connector.getProtocolHandler()).setSecretRequired(false);
+        
+        return connector;
+    }
+}
---
+++ src/main/java/com/trader/util/CacheUtil.java
@@ -0,0 +1,55 @@
+package com.trader.util;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.trader.entity.db.StockMst;
+import com.trader.repositories.StockMstRepository;
+
+
+
+@Service
+@CacheConfig(cacheNames = {"stockMstInfo", "stockMstMap"})
+public class CacheUtil {
+
+	@Autowired
+	CacheManager cacheManager;
+
+	@Autowired
+	protected StockMstRepository stockMstRepository;
+
+	@Scheduled(cron = "0 0 * * * *")
+	public void evictAllcachesAtIntervalsAll() {
+		for(String nm : cacheManager.getCacheNames()) {
+			cacheManager.getCache(nm).clear();
+		}
+	}
+
+	@Cacheable(value="stockMstMap", sync=true)
+	public StockMst getStockInfo(int code) {
+		return stockMstRepository.findByCode(code);
+	}
+	
+	@Cacheable(value="stockMstMap", sync=true)
+	public Map<Integer, StockMst> getStockMap() {
+		Map<Integer, StockMst> result = new TreeMap<Integer, StockMst>();
+
+		for(StockMst mst : stockMstRepository.findAll()) {
+			result.put(mst.getCode(), mst);
+		}
+		
+		return result;
+	}
+
+}
