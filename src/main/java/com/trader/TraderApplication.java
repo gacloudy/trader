a43e421af12a341477750b17ba54c6b6ec4d3a9d
gaclo <gaclo@192.168.0.13>
2022-02-26 00:08:21 +0900

    aa

--- src/main/java/com/trader/TraderApplication.java
+++ src/main/java/com/trader/TraderApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TraderApplication {
 
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(TraderApplication.class, args);
 	}
